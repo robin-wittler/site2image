684940222af73956884891b0a8fb066967b3114e
Robin Wittler <real@the-real.org>
2010-06-08 16:18:55 +0200

    fixed coding param

--- lib/application.py
+++ lib/application.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 import pygtk
 pygtk.require('2.0')
--- lib/browser.py
+++ lib/browser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 __author__ = 'Robin Wittler'
 __contact__ = 'real@the-real.org'
